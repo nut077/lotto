d623565d2eec128f610a507770c1f507cc7b9513
NuttapoT <nuttapon.tavee@gmail.com>
2020-01-02 10:21:00 +0700

    update baseMapper

--- src/main/java/com/github/nut077/lotto/dto/mapper/BaseMapper.java
+++ src/main/java/com/github/nut077/lotto/dto/mapper/BaseMapper.java
@@ -1,6 +1,7 @@
 package com.github.nut077.lotto.dto.mapper;
 
 import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.MappingTarget;
 
 import java.util.Collection;
 import java.util.List;
@@ -11,4 +12,5 @@ public interface BaseMapper<E, D> {
   List<D> mapToListDto(Collection<E> entity);
   @InheritInverseConfiguration
   E mapToEntity(D dto);
+  E mapToEntity(D dto, @MappingTarget E entity);
 }
--- src/main/java/com/github/nut077/lotto/dto/mapper/PeriodMapper.java
+++ src/main/java/com/github/nut077/lotto/dto/mapper/PeriodMapper.java
@@ -4,11 +4,8 @@ import com.github.nut077.lotto.dto.PeriodDto;
 import com.github.nut077.lotto.entity.Period;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring",
   injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PeriodMapper extends BaseMapper<Period, PeriodDto> {
-
-  Period mapToEntity(PeriodDto dto, @MappingTarget Period period);
 }
